29b77f520a558ae5e07edc9d3f69fc1046c07f36
vishal_vr3 <vishal_vr3@yahoo.in>
2022-05-10 12:51:58 +0530

    adding syntax page

---
+++ Questions/syntax.js
@@ -0,0 +1,32 @@
+/****** Arrow Function *****/
+
+/*
+const variable =  (parameter1, parameter2) => {
+  // Block of code
+}
+functionName(argument1, argument2); //calling Function
+*/
+
+// E.g, write a function that adds two numbers
+const sum = (num1, num2) => {
+  console.log(num1 + num2);
+};
+sum(2, 4); // 2 + 4 = 6
+sum(10, 20); // 10 + 20 = 30
+
+/********************************************************************* */
+/* E.g, write a function that adds only even
+Number between two given numbers.
+*/
+// function addEven(num1, num2) {
+//   let sum = 0;
+
+//   for (let i = num1; i <= num2; i++) {
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//   }
+
+//   console.log(sum);
+// }
+// addEven(2, 10); // 2 + 4 + 6 + 8 + 10 = 30
